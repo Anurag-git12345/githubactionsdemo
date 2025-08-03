fb460e03ac522a25695daf682139dc42d9bb2cd1
Anurag Katare <katareanurag1999@gmail.com>
2025-08-03 14:21:07 +0530

    Fix: Use JUnit 4 test format

---
+++ src/test/java/com/example/githubactions/HelloTest.java
@@ -0,0 +1,13 @@
+package com.example.githubactions;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class HelloTest {
+
+    @Test
+    public void testSum() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+}
